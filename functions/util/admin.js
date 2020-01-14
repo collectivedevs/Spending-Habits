c1a99dce15b972249230e1d3b529b319c40d17d0
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2020-01-13 21:04:25 -0400

    base project

---
+++ functions/util/admin.js
@@ -0,0 +1,13 @@
+const admin = require("firebase-admin");
+
+// Service Account Information - https://cloud.google.com/iam/docs/service-accounts?_ga=2.150185712.-794759788.1568568960
+//const serviceAccount = require("../util/keys/nakama-treehouse-c0de79946801.json");
+const { firebaseServiceAccount } = require("../util/config");
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseServiceAccount),
+  storageBucket: "gs://nakama-treehouse.appspot.com"
+});
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
