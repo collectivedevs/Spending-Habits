1ef5ce7481bfcba3007bc49e675cb619290f1220
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2020-01-29 23:01:44 -0400

    created login fuctionality.

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -37,5 +37,18 @@ const isEmpty = string => {
     };
   };
   
+  /** Validates Login Data. */
+
+exports.validateLoginData = data => {
+  let errors = {};
+
+  if (isEmpty(data.email)) errors.email = "Must not be empty";
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false
+  };
+};
   
   
