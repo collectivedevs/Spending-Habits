037a745b447ffa2bedc60be62efe15032b7787db
RichProsper <r.stp.p@hotmail.com>
2020-05-26 00:29:05 -0400

    [WIP] Design Modifications

--- client/src/components/layouts/Founders.js
+++ client/src/components/layouts/Founders.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import $ from 'jquery'
 import { Fab } from '@material-ui/core'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
@@ -19,12 +20,13 @@ const useStyles = makeStyles(theme => ({
         fontFamily: '"Lato", sans-serif',
         zIndex: 2,
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.5)',
+        overflow: 'hidden',
     },
     the_founders_wrapper: {
         backgroundColor: '#152433',
         width: '100%',
         height: '100px',
-        position: 'absolute'
+        position: 'absolute',
     },
     the_founders: {
         textAlign: 'center',
@@ -37,14 +39,16 @@ const useStyles = makeStyles(theme => ({
     },
     founders_images: {
         position: 'absolute',
-        left: '0%',
-        right: '0%',
+        width: '100%',
         top: '100px',
-        minHeight: 'calc(100% - 203px)',
+        minHeight: 'calc(100% - 178px)',
         backgroundColor: '#CCC',
         pointerEvents: 'none',
+        transitionDuration: '0.5s',
+        transitionTimingFunction: 'linear',
     },
     foun_1: {
+        left: '0%',
         background: `url(${foun_1})`,
         backgroundAttachment: 'fixed',
         backgroundPosition: 'center',
@@ -52,6 +56,7 @@ const useStyles = makeStyles(theme => ({
         backgroundSize: 'cover',
     },
     foun_2: {
+        left: '100%',
         background: `url(${foun_2})`,
         backgroundAttachment: 'fixed',
         backgroundPosition: 'center',
@@ -59,6 +64,7 @@ const useStyles = makeStyles(theme => ({
         backgroundSize: 'cover',
     },
     foun_3: {
+        left: '200%',
         background: `url(${foun_3})`,
         backgroundAttachment: 'fixed',
         backgroundPosition: 'center',
@@ -68,24 +74,30 @@ const useStyles = makeStyles(theme => ({
     display_none: {
         display: 'none',
     },
-    divider: {
+    seek_bar: {
         position: 'absolute',
         left: '0%',
         right: '0%',
-        top: 'calc(100% - 103px)',
+        top: 'calc(100% - 78px)',
         height: '3px',
-        backgroundColor: '#DC004E',
+        backgroundColor: '#F00',
+        "&:hover": {
+            cursor: 'pointer',
+        },
     },
     seek: {
-        display: 'none',
         position: 'absolute',
         left: '0%',
         right: '0%',
-        top: 'calc(100% - 109px)',
+        top: 'calc(100% - 84px)',
         height: '15px',
         width: '15px',
         borderRadius: '50%',
-        backgroundColor: '#DC004E',
+        backgroundColor: '#F00',
+        zIndex: 3,
+        "&:hover": {
+            cursor: 'pointer',
+        },
     },
     prev_next: {
         position: 'absolute',
@@ -103,41 +115,83 @@ const useStyles = makeStyles(theme => ({
     next: {
         right: theme.spacing(2),
     },
-    slides_wrapper: {
-
+    dots_wrapper: {
+        backgroundColor: '#152433',
+        width: '100%',
+        height: '75px',
+        position: 'absolute',
+        top: 'calc(100% - 75px)',
+        textAlign: 'center',
     },
-    slides: {
-
+    dots: {
+        cursor: 'pointer',
+        height: '16px',
+        width: '16px',
+        margin: '29.5px 1.5%',
+        backgroundColor: '#FFF',
+        borderRadius: '50%',
+        display: 'inline-block',
+        "&:hover": {
+            backgroundColor: '#832bff',
+            animation: '$pulsePurple 1.5s 0s ease-out infinite',
+        },
     },
-    slides_active: {
-
+    dots_active: {
+        backgroundColor: '#F00',
+        animation: '$pulseRed 1.5s 0s ease-out infinite',
+    },
+    "@keyframes pulseRed": {
+        '0%': {
+            boxShadow: '0 0 4px 3px rgba(255, 0, 0, 0), 0 0 0px 0px #152433, 0 0 0px 0px rgba(255, 0, 0, 0)'
+        },
+        '10%': {
+            boxShadow: '0 0 4px 3px #F00, 0 0 6px 5px #152433, 0 0 6px 7px #F00'
+        },
+        '100%': {
+            boxShadow: '0 0 4px 3px rgba(255, 0, 0, 0), 0 0 0px 20px #152433, 0 0 0px 20px rgba(255, 0, 0, 0)'
+        },
+    },
+    "@keyframes pulsePurple": {
+        '0%': {
+            boxShadow: '0 0 4px 3px rgba(0, 0, 0, 0), 0 0 0px 0px #152433, 0 0 0px 0px rgba(0, 0, 0, 0)'
+        },
+        '10%': {
+            boxShadow: '0 0 4px 3px #832bff, 0 0 6px 5px #152433, 0 0 6px 7px #832bff'
+        },
+        '100%': {
+            boxShadow: '0 0 4px 3px rgba(0, 0, 0, 0), 0 0 0px 20px #152433, 0 0 0px 20px rgba(0, 0, 0, 0)'
+        },
     },
 }))
 
 function Founders() {
     const classes = useStyles()
+    const len = (3 - 1)
+    const imgsLeft = [0, 100, 200]
     const [index, setIndex] = React.useState(0)
-    const images = [classes.foun_1, classes.foun_2, classes.foun_3]
-    const len = images.length
 
-    const changeImage = change => {
-        if ((index + change) > (len - 1)) {
-            document.getElementsByClassName(images[len - 1])[0].classList.toggle(classes.display_none)
-            document.getElementsByClassName(images[0])[0].classList.toggle(classes.display_none)
-            setIndex(0)
+    useEffect(() => {
+        if (index > 0) $(`.${classes.prev}`).removeClass(classes.display_none)
+        else $(`.${classes.prev}`).addClass(classes.display_none)
+
+        if (index < len) $(`.${classes.next}`).removeClass(classes.display_none)
+        else $(`.${classes.next}`).addClass(classes.display_none)
+
+        // Change Active Dot
+        let dots = document.getElementsByClassName(classes.dots)
+        for (let i = 0; i < dots.length; i++) {
+            if (index === i) dots[i].classList.add(classes.dots_active)
+            else dots[i].classList.remove(classes.dots_active)
         }
-        else if ((index + change) < 0) {
-            document.getElementsByClassName(images[1])[0].classList.toggle(classes.display_none)
-            document.getElementsByClassName(images[len - 1])[0].classList.toggle(classes.display_none)
-            setIndex(len - 1)
+
+        // Change Image
+        let images = document.getElementsByClassName(classes.founders_images)
+        for (let i = 0; i < images.length; i++) {
+            images[i].style.left = (imgsLeft[i] - (index * 100)) + '%'
         }
-        else {
-            document.getElementsByClassName(images[index])[0].classList.toggle(classes.display_none)
-            document.getElementsByClassName(images[index + change])[0].classList.toggle(classes.display_none)
-            setIndex(index + change)
-        }
-        console.log(index)
-    }
+    },
+    [index] // Only re-run the effect if index changes
+    )
 
     return (
         <div id="founders" className={classes.founders_container}>
@@ -145,13 +199,13 @@ function Founders() {
                 <h3 className={classes.the_founders}>The Founders</h3>
             </div>
             <div className={`${classes.founders_images} ${classes.foun_1}`}></div>
-            <div className={`${classes.founders_images} ${classes.foun_2} ${classes.display_none}`}></div>
-            <div className={`${classes.founders_images} ${classes.foun_3} ${classes.display_none}`}></div>
+            <div className={`${classes.founders_images} ${classes.foun_2}`}></div>
+            <div className={`${classes.founders_images} ${classes.foun_3}`}></div>
             <Fab
                 size="small"
                 aria-label="prev"
-                className={`${classes.prev_next} ${classes.prev}`}
-                onClick={() => {changeImage(-1)}}
+                className={`${classes.prev_next} ${classes.prev} ${classes.display_none}`}
+                onClick={() => {setIndex(index - 1)}}
             >
                 <ChevronLeftIcon />
             </Fab>
@@ -159,16 +213,16 @@ function Founders() {
                 size="small"
                 aria-label="next"
                 className={`${classes.prev_next} ${classes.next}`}
-                onClick={() => {changeImage(1)}}
+                onClick={() => {setIndex(index + 1)}}
             >
                 <ChevronRightIcon />
             </Fab>
-            <div className={classes.divider}></div>
+            <div className={classes.seek_bar}></div>
             <div className={classes.seek}></div>
-            <div className={classes.slides_wrapper}>
-                <div className={classes.slides}></div>
-                <div className={classes.slides}></div>
-                <div className={classes.slides}></div>
+            <div className={classes.dots_wrapper}>
+                <div className={classes.dots} onClick={() => {setIndex(0)}}></div>
+                <div className={classes.dots} onClick={() => {setIndex(1)}}></div>
+                <div className={classes.dots} onClick={() => {setIndex(2)}}></div>
             </div>
         </div>
     )
